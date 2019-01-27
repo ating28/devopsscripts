f5e557c3e4976214a03cdca7212c918bd66a7c5d
Nicholas Reith <nreith@users.noreply.github.com>
2019-01-27 11:40:06 -0600

    Update mlserver9.3.0.sh

--- mlserver9.3.0.sh
+++ mlserver9.3.0.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Run script as root/sudo
+# Optinonally use grep -v '# R$' for example to filter out R, Python or Server specific packages if you don't need those
 
 # ML SERVER 9.3.0 INSTALL
 ####################################################################################
 
 # Paths
-echo "export LD_LIBRARY_PATH=/opt/microsoft/mlserver/9.3.0/runtime/R/lib:$LD_LIBRARY_PATH" >> /home/$USER/.bashrc
-echo "export PATH=$PATH:/opt/microsoft/mlserver/9.3.0/runtime/python/bin/" >> /home/$USER/.bashrc
-echo "export PATH=$PATH:/opt/microsoft/mlserver/9.3.0/runtime/R/bin/" >> /home/$USER/.bashrc
+echo "export LD_LIBRARY_PATH=/opt/microsoft/mlserver/9.3.0/runtime/R/lib:$LD_LIBRARY_PATH" >> /home/$USER/.bashrc   # R
+echo "export PATH=$PATH:/opt/microsoft/mlserver/9.3.0/runtime/python/bin/" >> /home/$USER/.bashrc                   # Python
+echo "export PATH=$PATH:/opt/microsoft/mlserver/9.3.0/runtime/R/bin/" >> /home/$USER/.bashrc                        # R
 
 # Build Deps
 apt-get -y update
@@ -26,27 +27,27 @@ apt-get -y update
 
 # R Stuff
 
-apt-get install -y microsoft-r-open-foreachiterators-3.4.3
-apt-get install -y microsoft-r-open-mkl-3.4.3
-apt-get install -y microsoft-r-open-mro-3.4.3
-apt-get install -y microsoft-mlserver-packages-r-9.3.0
-apt-get install -y microsoft-mlserver-mml-r-9.3.0
-apt-get install -y microsoft-mlserver-mlm-r-9.3.0
-R CMD javareconf
+apt-get install -y microsoft-r-open-foreachiterators-3.4.3  # R
+apt-get install -y microsoft-r-open-mkl-3.4.3               # R
+apt-get install -y microsoft-r-open-mro-3.4.3               # R
+apt-get install -y microsoft-mlserver-packages-r-9.3.0      # R
+apt-get install -y microsoft-mlserver-mml-r-9.3.0           # R
+apt-get install -y microsoft-mlserver-mlm-r-9.3.0           # R
+R CMD javareconf # R
 
 # Python Stuff
-apt-get install -y microsoft-mlserver-python-9.3.0
-apt-get install -y microsoft-mlserver-packages-py-9.3.0
-apt-get install -y microsoft-mlserver-mml-py-9.3.0
-apt-get install -y microsoft-mlserver-mlm-py-9.3.0
+apt-get install -y microsoft-mlserver-python-9.3.0          # Python
+apt-get install -y microsoft-mlserver-packages-py-9.3.0     # Python
+apt-get install -y microsoft-mlserver-mml-py-9.3.0          # Python
+apt-get install -y microsoft-mlserver-mlm-py-9.3.0          # Python
 
 # Other Stuff
 apt-get install -y azure-cli=2.0.26-1~xenial
 apt-get install -y dotnet-runtime-2.0.0
-apt-get install -y microsoft-mlserver-adminutil-9.3.0
-apt-get install -y microsoft-mlserver-config-rserve-9.3.0
-apt-get install -y microsoft-mlserver-computenode-9.3.0
-apt-get install -y microsoft-mlserver-webnode-9.3.0
+apt-get install -y microsoft-mlserver-adminutil-9.3.0       # Server
+apt-get install -y microsoft-mlserver-config-rserve-9.3.0 
+apt-get install -y microsoft-mlserver-computenode-9.3.0     # Server
+apt-get install -y microsoft-mlserver-webnode-9.3.0         # Server
 
 # Activate
 /opt/microsoft/mlserver/9.3.0/bin/R/activate.sh
@@ -55,13 +56,13 @@ apt-get install -y microsoft-mlserver-webnode-9.3.0
 apt list --installed | grep microsoft
 
 # Choose a package name and obtain verbose version information
-dpkg --status microsoft-mlserver-packages-r-9.3.0
-dpkg --status microsoft-mlserver-packages-py-9.3.0
+dpkg --status microsoft-mlserver-packages-r-9.3.0           # R
+dpkg --status microsoft-mlserver-packages-py-9.3.0          # Python
 
 # Make ML Server Python the Default Python/R
-echo 'alias python=mlserver-python' >> /home/$USER/.bashrc
-echo 'alias python3=mlserver-python' >> /home/$USER/.bashrc
-echo 'alias R=Revo64' >> /home/$USER/.bashrc
+echo 'alias python=mlserver-python' >> /home/$USER/.bashrc  # Python
+echo 'alias python3=mlserver-python' >> /home/$USER/.bashrc # Python
+echo 'alias R=Revo64' >> /home/$USER/.bashrc                # R
 source /home/$USER/.bashrc
 
 # Cleanup
