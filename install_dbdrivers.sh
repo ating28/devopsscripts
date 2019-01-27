482e8f9314e2491e3bbb9dd6a737f62f10c5af15
Nicholas Reith <nreith@users.noreply.github.com>
2019-01-26 21:45:36 -0600

    Update install_dbdrivers.sh

--- install_dbdrivers.sh
+++ install_dbdrivers.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-# BUILD DEPS
-############
+# Run script as root/sudo
+
+# DB DRIVERS INSTALL
+####################################################################################
+
+# Build Deps
 apt-get clean -y
 apt-get update -y
 apt-get install -y curl wget apt-transport-https nano unzip
 
-# MS SQL SERVER DRIVERS
-#######################
+# MS SQL Server Drivers
 cd /tmp && curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 apt-get update -y
 ACCEPT_EULA=Y apt-get install -y --no-install-recommends msodbcsql17
 # optional: for bcp and sqlcmd
 ACCEPT_EULA=Y apt-get install -y --no-install-recommends mssql-tools
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /etc/environment
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/$USER/.bashrc
 # optional: for unixODBC development headers
 apt-get install -y --no-install-recommends unixodbc-dev
 # Don't think we need these below
@@ -23,8 +26,7 @@ apt-get install -y --no-install-recommends unixodbc-dev
     # ln -s libssl.so.1.0.0 /lib/x86_64-linux-gnu/libssl.so.10
     # ln -s libodbcinst.so.2 /usr/lib/x86_64-linux-gnu/libodbcinst.so.1
 
-# TERADATA JDBC/ODBC DRIVERS
-############################
+# Teradata JDBC/ODBC
 mkdir /opt/terajdbc && cd /tmp
 wget https://s3.amazonaws.com/domino-dell-packages/tdodbc1620__ubuntu_indep.16.20.00.18-1.tar.gz
 wget https://s3-us-west-2.amazonaws.com/cs-dell/teradata/terajdbc4.jar -O /opt/teradata/jdbc/terajdbc4.jar
@@ -33,8 +35,7 @@ tar -xf tdodbc1620__ubuntu_indep.16.20.00.18-1.tar.gz
 apt-get -f install -y --no-install-recommends lib32stdc++6 lib32gcc1 libc6-i386
 cd /tmp/tdodbc1620 && dpkg -i tdodbc1620-16.20.00.18-1.noarch.deb
 
-# ORACLE INSTANTCLIENT
-######################
+# Oracle Instantclient
 cd /tmp
 wget https://s3-us-west-2.amazonaws.com/domino-deployment/2016/02/22/instantclient-basic-linux.x64-12.1.0.2.0.zip
 wget https://s3-us-west-2.amazonaws.com/domino-deployment/2016/02/22/instantclient-sdk-linux.x64-12.1.0.2.0.zip
@@ -48,19 +49,17 @@ ln -s /opt/oracle/instantclient_12_1/libclntsh.so.12.1 /opt/oracle/libclntsh.so 
 ln -s /opt/oracle/instantclient_12_1/libocci.so.12.1 /opt/oracle/libocci.so && \
 ln -s /opt/oracle/instantclient_12_1/libociei.so /opt/oracle/libociei.so && \
 ln -s /opt/oracle/instantclient_12_1/libnnz12.so /opt/oracle/libnnz12.so
-echo "export ORACLE_BASE=/usr/lib/instantclient_12_1" >> /etc/environment
-echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/instantclient_12_1" >> /etc/environment
-echo "export TNS_ADMIN=/usr/lib/instantclient_12_1" >> /etc/environment
-echo "export ORACLE_HOME=/usr/lib/instantclient_12_1" >> /etc/environment
+echo "export ORACLE_BASE=/usr/lib/instantclient_12_1" >> /home/$USER/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/instantclient_12_1" >> /home/$USER/.bashrc
+echo "export TNS_ADMIN=/usr/lib/instantclient_12_1" >> /home/$USER/.bashrc
+echo "export ORACLE_HOME=/usr/lib/instantclient_12_1" >> /home/$USER/.bashrc
 
-# POSTGRESQL CLIENT
-###################
+# Postgresql Client
 echo "deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main" > /etc/apt/sources.list.d/postgres.list
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update && apt-get install -y postgresql-10 postgresql-client-10 postgresql-contrib-10
 
-# ODBCINST.INI
-##############
+# odbcinst.ini
 printf '
 [FreeTDS]
 UsageCount=2
@@ -85,6 +84,6 @@ Description=Oracle JDBC driver for Oracle 12.1
 Driver = /opts/oracle/instantclient_12_1/ojdbc7.jar
 ' >> /etc/odbcinst.ini
 
-# TMP FILES CLEANUP
-###################
+# Cleanup
+apt-get clean -y && apt-get autoremove -y
 rm -rf /tmp/*
