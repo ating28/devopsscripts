a8dc2bf47562e1106a771e23e40b36328c66c6d2
Nicholas Reith <nreith@users.noreply.github.com>
2019-02-20 22:12:57 -0600

    Update and rename mssqlserver17.sh to mssqlserver.sh

--- dbdrivers/mssqlserver.sh
+++ dbdrivers/mssqlserver.sh
@@ -21,6 +21,10 @@ apt-get install -y --no-install-recommends unixodbc-dev
     # ln -s libcrypto.so.1.0.0 /lib/x86_64-linux-gnu/libcrypto.so.10
     # ln -s libssl.so.1.0.0 /lib/x86_64-linux-gnu/libssl.so.10
     # ln -s libodbcinst.so.2 /usr/lib/x86_64-linux-gnu/libodbcinst.so.1
+
+# Get JDBC Driver
+cd /opt/microsoft && mkdir -p msjdbcsql7 && cd msjdbcsql7
+wget https://github.com/Microsoft/mssql-jdbc/releases/download/v7.2.1/mssql-jdbc-7.2.1.jre8.jar
     
 printf '
 [ODBC Driver 17 for SQL Server]
@@ -29,4 +33,7 @@ Driver=/opt/microsoft/msodbcsql17/lib64/libmsodbcsql-17.2.so.0.1
 Threading=1
 UsageCount=1
 
+[JDBC Driver 7 for SQL Server]
+Description=Microsoft JDBC Driver 7 for SQL Server
+Driver=/opt/microsoft/msjdbcsql7/mssql-jdbc-7.2.1.jre8.jar
 ' >> /etc/odbcinst.ini
