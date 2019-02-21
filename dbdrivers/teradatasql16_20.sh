c3d9a6742f60fa275f87ee11dbaa0a099b231f7f
Nicholas Reith <nreith@users.noreply.github.com>
2019-02-21 09:41:56 -0600

    Update teradatasql16_20.sh

--- dbdrivers/teradatasql16_20.sh
+++ dbdrivers/teradatasql16_20.sh
@@ -16,12 +16,12 @@ apt-get -f install -y --no-install-recommends lib32stdc++6 lib32gcc1 libc6-i386
 cd /tmp/tdodbc1620 && dpkg -i tdodbc1620-16.20.00.18-1.noarch.deb
 
 printf '
-[Teradata Database ODBC Driver 16.20]
+[Teradata ODBC Driver 16.20]
 Description=Teradata Database ODBC Driver 16.20
 Driver=/opt/teradata/client/ODBC_64/lib/tdataodbc_sb64.so
 # Note: Currently, Data Direct Driver Manager does not support Connection Pooling feature.
 
-[Teradata Database JDBC Driver 4]
+[Teradata JDBC Driver 4]
 Description=Teradata Database JDBC Driver 4
 Driver=/opt/teradata/jdbc/terajdbc4.jar
 
